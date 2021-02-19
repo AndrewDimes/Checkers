ddca8e44cfcca03a3d18ccd403310d05957ffac8
Andrew Dimes <andrewdimes@gmail.com>
2021-02-19 18:03:00 -0500

    added pieces & background

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+const blackPieces = 12
+const redPieces = 12
+/*----- app's state (variables) -----*/
+let redGrave;
+let blackGrave;
+let winner;
+
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
