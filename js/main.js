771e13c604deb946c9777e4f7415957441cb9d5b
Andrew Dimes <andrewdimes@gmail.com>
2021-02-20 15:06:02 -0500

    changed font

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ let emptyDiv = [];
 let occupiedRed = [];
 let occupiedBlack =[];
 let turn = 0;
-let redScore = 12;
-let blackScore =12;
+
 
 /*----- cached element references -----*/
 const startButton = document.getElementById('button');
@@ -14,6 +13,8 @@ const boardEl = document.querySelectorAll('.x');
 const blackPieces = document.querySelectorAll('#black');
 const redPieces = document.querySelectorAll('#red');
 const playerTurnText = document.getElementById('players-turn');
+const redScore = document.getElementById('red-points')
+const blackScore = document.getElementById('black-points')
 
 
 /*----- event listeners -----*/
@@ -60,12 +61,19 @@ function playerMove() {
             blackPieces[i].style.border = '3px solid gold';
             blackPieces[i].style.boxShadow = '0px 0px 20px gold';
             blackPieces[i].style.borderRadius = '30px';
+            blackScore.style.border = '3px solid gold';
+            blackScore.style.boxShadow = '0px 0px 20px gold';
+            blackScore.style.borderRadius = '30px';
+            
         } 
     }else{
         for(let i = 0; i < redPieces.length; i++ ){
             redPieces[i].style.border = '3px solid gold';
             redPieces[i].style.boxShadow = '0px 0px 20px gold';
             redPieces[i].style.borderRadius = '30px';
+            redScore.style.border = '3px solid gold';
+            redScore.style.boxShadow = '0px 0px 20px gold';
+            redScore.style.borderRadius = '30px';
         };
     };
 };
